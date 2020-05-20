23857d2d7da902d6d9f9dffd4d64257a27ba25d1
采芙 李 <qeeee7765@ccf.org.tw>
2020-05-20 11:24:41 +0800

    條件判斷、迴圈、集合

---
+++ Lesson5Statement/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region condition
+            bool condition = true;
+            if (condition)
+            {
+                Console.WriteLine("condition is true");
+            }
+
+            int ten = 10;
+            if (ten > 0)
+            {
+                Console.WriteLine("ten > 0");
+            }
+
+            if (!false)
+            {
+                Console.WriteLine("not false");
+            }
+
+            if (!false && false)  //&&:and , ||:or   //& , | 兩者都會運算，會出現例外錯誤
+            {
+                Console.WriteLine("True");
+            }
+
+
+
+            if (ten > 100)
+            {
+                Console.WriteLine("ten > 100");
+                if (true)
+                {
+                    Console.WriteLine("true and true");
+                }
+            }
+            else if (ten > 5)
+            {
+                Console.WriteLine("ten > 5 and ten <= 100");
+            }
+            else
+            {
+                Console.WriteLine("ten <= 5");
+            }
+
+            //?: 運算子
+            ten = 101;
+            int ifelse = ten < 100 ? ten : 99;
+            Console.WriteLine(ifelse);
+
+            int switchKey = 100;
+            switch (switchKey)
+            {
+                case 10:
+                    Console.WriteLine("switchKey is 10");
+                    break;
+                case 100:
+                    Console.WriteLine("switchKey is 100");
+                    break;
+                default:
+                    Console.WriteLine("I don't know switchKey");
+                    break;
+            }
+            #endregion
+            int i;
+            int j = 10;
+            for(i = 0, Console.WriteLine("Start:{0}", i); i < j; i++, j--, Console.WriteLine("i={0}, j={1}", i, j))
+            {
+                Console.WriteLine("Body of the loop");
+            }
+
+            bool stop = false;
+            for(; !stop; )
+            {
+                stop = true;
+                Console.WriteLine("Can u stop");
+            }
+
+            List<int> listInt = new List<int>() { 1, 2, 3 };
+            foreach (var intInList in listInt)
+            {
+                Console.WriteLine(intInList);
+            }
+
+            int n = 1;
+            //n++ 未加前先判斷
+            //++n 加完後判斷
+            while (n++ < 6)  
+            {
+                Console.WriteLine("n is {0}", n);
+            }
+
+            do
+            {
+                Console.WriteLine("time");
+            }
+            while (false);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson6Collection/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array
+
+            int[] numbers = new int[5];
+            string[,] names = new string[5, 4];
+            byte[][] scores = new byte[5][];
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = new byte[i + 3];
+            }
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("Length of row {0} is {1}", i, scores[i].Length);
+            }
+
+            int[] numbers2 = new int[5] { 1, 2, 3, 4, 5 };
+            int[] numbers3 = new int[] { 1, 2, 3, 4, 5 };
+            int[] numbers4 = { 1, 2, 3, 4, 5 };
+
+            string[,] names2 = { { "g", "k" }, { "h", "j" } };
+            int[][] numberss = { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 } };
+
+            Console.WriteLine(numbers2[2]);
+            Console.WriteLine(numbers2.Length);
+
+            foreach(int i in numbers2)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+            //System.Collections
+            //ArrayList 可儲存多種不同類型的元素
+            ArrayList al = new ArrayList();
+            al.Add(5);
+            al.Add(100);
+            al.Remove(5);
+            al.Add("jikexueyuan");
+
+            foreach(var e in al)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine(al[0]);
+
+            //List 只能儲存同一類型的元素
+            List<int> intList = new List<int>();
+            intList.Add(500);
+            intList.AddRange(new int[] { 501, 502 });
+            Console.WriteLine(intList.Contains(200));
+            Console.WriteLine(intList.IndexOf(501));
+            intList.Remove(501);
+            intList.Insert(1, 1001);
+            
+            //Hashtable 沒有強制規定類型
+            Hashtable ht = new Hashtable();
+            ht.Add("first", "jike");
+            ht.Add("second", "xueyuan");
+            ht.Add(100, 100);
+            Console.WriteLine(ht["second"]);
+            Console.WriteLine(ht[100]);
+
+            //Dictionary 只能儲存同一類型，線程不是安全
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            d.Add("first", "jike");
+            //線程安全->ConcurrentDictionary
+
+            //經過排序的List，根據key進行排序
+            SortedList<int, int> sl = new SortedList<int, int>();
+            sl.Add(5, 105);
+            sl.Add(2, 102);
+            sl.Add(10, 99);
+            foreach(var sle in sl)
+            {
+                Console.WriteLine(sle.Value);
+            }
+
+            //stack 先進後出, queue 先進先出
+            Console.ReadLine();
+        }
+    }
+}
